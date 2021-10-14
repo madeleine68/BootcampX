d897504782db9b4022f406f46b1031df21accc26
Madeleine <mahdieh.moghadasi@gmail.com>
2021-10-13 21:09:44 -0400

    parameterized queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -6,16 +6,18 @@ const pool = new Pool({
     host: 'localhost',
     database: 'bootcampx'
 });
-
-pool.query(`
+const cohortName = process.argv[2] || 'JUL02'
+values = [`%${cohortName}%`]
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teachers.id = teacher_id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${process.argv[2] || 'JUL02'}%'
+WHERE cohorts.name LIKE $1
 ORDER BY teachers.name
-`)
+`
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort}: ${user.teacher}`);
